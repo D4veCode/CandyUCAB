7baa9d31e0060314aecafda079df956c60ce6c4e
Franco Marino <franco2662@gmail.com>
2018-06-16 00:36:42 -0400

    Añadidos algunos inserts

---
+++ Queryngs/Insert_TipoProducto.sql
@@ -0,0 +1,9 @@
+
+Insert into tipo_producto(id,nombre)values(1,'Caramelo blando');
+Insert into tipo_producto(id,nombre)values(2,'Caramelo duro');
+Insert into tipo_producto(id,nombre)values(3,'Chupeta');
+
+Select * from tipo_producto;
+
+
+
