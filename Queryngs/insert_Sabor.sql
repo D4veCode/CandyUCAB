b8f1519dda20afc6d13e7f59a99e8e7f730bd3c8
Franco Marino <franco2662@gmail.com>
2018-06-16 01:24:50 -0400

    Añadidos inserts

--- Queryngs/insert_Sabor.sql
+++ Queryngs/insert_Sabor.sql
@@ -1,15 +1,21 @@
-Insert into producto(id,nombre,precio,descripcion,fk_tprod)Values(1,'Chupeta Rimbombin',35,'Deliciosa chupeta rellena envolvuelta con la forma de margarita','Chupeta');
-Insert into producto(id,nombre,precio,descripcion,fk_tprod)Values(2,'Ricolin',20,'Caramelo de bolsillo que posee un sabor clasico mentolado y citrico','Caramelo duro');
-Insert into producto(id,nombre,precio,descripcion,fk_tprod)Values(3,'Firi Firi',20,'Pastillas que proporcionan frescura al instante','Caramelo duro');
-Insert into producto(id,nombre,precio,descripcion,fk_tprod)Values(4,'Chupeta de Corazon',30,'La tipica chupeta de fresa con forma de corazon','Chupeta');
-Insert into producto(id,nombre,precio,descripcion,fk_tprod)Values(5,'Pirulitos',30,'Caramelos blandos y alargados','Caramelo blando');
-Insert into producto(id,nombre,precio,descripcion,fk_tprod)Values(6,'Ricura',25,'Bola de caramelo con centro de chicle','Caramelo blando');
-Insert into producto(id,nombre,precio,descripcion,fk_tprod)Values(7,'Original',25,'El caramelo balsamico de Candy UCAB','Caramelo blando');
-Insert into producto(id,nombre,precio,descripcion,fk_tprod)Values(8,'Dulcin',20,'Caramelos masticables de forma cuadrada','Caramelo blando');
-Insert into producto(id,nombre,precio,descripcion,fk_tprod)Values(9,'Choco Candy',40,'Caramelos de chocolate','Caramelo duro');
-Insert into producto(id,nombre,precio,descripcion,fk_tprod)Values(10,'Blue',40,'Caramelos artesanal','Caramelo duro');
-Select * from producto;
-
-
-
-
+Insert into sabor(id,nombre)values(1,'Menta');
+Insert into sabor(id,nombre)values(2,'Fresa');
+Insert into sabor(id,nombre)values(3,'Lima');
+Insert into sabor(id,nombre)values(4,'Naranja');
+Insert into sabor(id,nombre)values(5,'Melocoton');
+Insert into sabor(id,nombre)values(6,'Patilla');
+Insert into sabor(id,nombre)values(7,'Manzana');
+Insert into sabor(id,nombre)values(8,'Eucalipto');
+Insert into sabor(id,nombre)values(9,'Limon');
+Insert into sabor(id,nombre)values(10,'Melisa');
+Insert into sabor(id,nombre)values(11,'Limon fresh');
+Insert into sabor(id,nombre)values(12,'Piña');
+Insert into sabor(id,nombre)values(13,'Cereza');
+Insert into sabor(id,nombre)values(14,'Choco tradicional');
+Insert into sabor(id,nombre)values(15,'Cafe expresso');
+Insert into sabor(id,nombre)values(16,'Capuccino');
+Insert into sabor(id,nombre)values(17,'Fresa con nata');
+Insert into sabor(id,nombre)values(18,'Nata');
+Insert into sabor(id,nombre)values(19,'Mousse de limon');
+Insert into sabor(id,nombre)values(20,'Fresa picante');
+Insert into sabor(id,nombre)values(21,'Miel');
