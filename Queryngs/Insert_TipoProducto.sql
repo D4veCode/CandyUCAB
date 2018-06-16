7d8499f7d39dac1e4c815f080521f0cb26bafa0b
Broncebat <32389647+ferrodv@users.noreply.github.com>
2018-06-16 01:33:52 -0400

    Update Insert_TipoProducto.sql

--- Queryngs/Insert_TipoProducto.sql
+++ Queryngs/Insert_TipoProducto.sql
@@ -1,10 +1,7 @@
 
-Insert into tipo_producto(id,nombre)values
-(1,'Caramelo blando'),
-(2,'Caramelo duro'),
-(3,'Chupeta');
+Insert into tipo_producto(nombre)values
+('Caramelo blando'),
+('Caramelo duro'),
+('Chupeta');
 
 Select * from tipo_producto;
-
-
-
