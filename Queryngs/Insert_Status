4df8f69959df4b7aa9e6f4c8b1506d1b49fce6af
Franco Marino <franco2662@gmail.com>
2018-06-18 00:34:31 -0400

    Insert de sucursales (40 Mini Candy Shop)

---
+++ Queryngs/Insert_Status
@@ -0,0 +1,3 @@
+Insert into status(id,tipo)values(1,'Listo para entrega'),(2,'Entregado');
+
+
---
+++ Queryngs/Insert_Sucursal.sql
@@ -0,0 +1,48 @@
+Insert into sucursal(cod,nombre,fk_lugar)values
+(1,'Mini Candy Shop',487+611),
+(2,'Mini Candy Shop',25+454),
+(3,'Mini Candy Shop',487+648),
+(4,'Mini Candy Shop',487+50),
+(5,'Mini Candy Shop',25+90),
+(6,'Mini Candy Shop',13),
+(7,'Mini Candy Shop',487+986),
+(8,'Mini Candy Shop',25+58),
+(9,'Mini Candy Shop',25+363),
+(10,'Mini Candy Shop',487+379),
+(11,'Mini Candy Shop',487+352),
+(12,'Mini Candy Shop',487+726),
+(13,'Mini Candy Shop',487+625),
+(14,'Mini Candy Shop',25+387),
+(15,'Mini Candy Shop',487+26),
+(16,'Mini Candy Shop',487+439),
+(17,'Mini Candy Shop',487+133),
+(18,'Mini Candy Shop',487+1111),
+(19,'Mini Candy Shop',487+635),
+(20,'Mini Candy Shop',487+384),
+(21,'Mini Candy Shop',487+1118),
+(22,'Mini Candy Shop',487+984),
+(23,'Mini Candy Shop',487+800),
+(24,'Mini Candy Shop',487+40),
+(25,'Mini Candy Shop',25+36),
+(26,'Mini Candy Shop',25+146),
+(27,'Mini Candy Shop',25+66),
+(28,'Mini Candy Shop',25+300),
+(30,'Mini Candy Shop',25+365),
+(31,'Mini Candy Shop',25+443),
+(32,'Mini Candy Shop',487+349),
+(33,'Mini Candy Shop',25+53),
+(34,'Mini Candy Shop',25+360),
+(35,'Mini Candy Shop',25+451),
+(36,'Mini Candy Shop',25+70),
+(37,'Mini Candy Shop',25+151),
+(38,'Mini Candy Shop',25+240),
+(39,'Mini Candy Shop',25+289),
+(40,'Mini Candy Shop',16),
+(41,'Mini Candy Shop',25+345);
+
+
+
+
+
+
+
