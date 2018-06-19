b1a54b8f4ce6c196275573ac83138ebf998a8ae7
Franco Marino <franco2662@gmail.com>
2018-06-19 02:34:30 -0400

    Insert Cliente Natural

--- Queryngs/INSERTS.sql
+++ Queryngs/INSERTS.sql
@@ -1597,7 +1597,11 @@ insert into sabor_producto(id,fk_producto,fk_sabor)values(45,10,'Capuccino');
 
 
 
-Insert into status(id,tipo)values(1,'Listo para entrega'),(2,'Entregado');
+Insert into status(id,tipo)values(('Por Empaquetar'),
+('Empaquetandose'),
+('Por Enviar'),
+('Enviandose'),
+('Entregado');
 
 
 Insert into sucursal(cod,nombre,fk_lugar)values
@@ -2047,6 +2051,95 @@ Insert into empleado(ci,nombre,nombre2,apellido,apellido2,salario,fk_lugar,fk_su
 (25161578,'Luis','Alejandro','Rosas','Da Silva',1400,487+394,39,'Empleado78'),
 (25171578,'Luis','Manuel','Sanchez','Gomez',1400,487+394,40,'Empleado79'),
 (25181578,'Ana','José','Lopez','Rosas',1400,487+394,40,'Empleado80');
+
+Insert into cliente_natural(rif,ci,num_carnet,email,nombre,apellido,apellido2,fk_usuario,fk_lugar)values
+('J20345678',20345678,'01-00000001','cliente1@gmail.com','Jesus','Rodriguez','Altuve','ClienteNatural1',487+168),
+('J21345678',21345678,'02-00000001','cliente2@gmail.com','Jesus','Rodriguez','Almeida','ClienteNatural2',487+168),
+('J22345678',22345678,'01-00000002','cliente3@gmail.com','Jesus','Rodriguez','Lopez','ClienteNatural3',487+168),
+('J23345678',23345678,'02-00000002','cliente4@gmail.com','Jesus','Pereira','Altuve','ClienteNatural4',487+168),
+('J24345678',24345678,'01-00000003','cliente5@gmail.com','Jesus','Gonzalez','Altuve','ClienteNatural5',487+168),
+('J25345678',25345678,'02-00000003','cliente6@gmail.com','Jesus','Perez','Altuve','ClienteNatural6',487+168),
+('J26345678',26345678,'01-00000004','cliente7@gmail.com','Jesus','Gonzalez','Rodriguez','ClienteNatural7',487+168),
+('J27345678',27345678,'02-00000004','cliente8@gmail.com','Jesus','Almeida','Gonzalez','ClienteNatural8',487+168),
+('J28345678',28345678,'01-00000005','cliente9@gmail.com','Jesus','Torrealba','Padron','ClienteNatural9',487+168),
+('J29345678',29345678,'02-00000005','cliente10@gmail.com','Jesus','Rodriguez','Altuve','ClienteNatural10',487+168),
+
+('J30345678',30345678,'01-00000006','cliente11@gmail.com','José','Rodriguez','Perez','ClienteNatural11',487+648),
+('J31345678',31345678,'02-00000006','cliente12@gmail.com','José','Rodriguez','Almeida','ClienteNatural12',487+648),
+('J32345678',32345678,'01-00000007','cliente13@gmail.com','José','Rodriguez','Andrade','ClienteNatural13',487+648),
+('J33345678',33345678,'02-00000007','cliente14@gmail.com','José','Pereira','Andrade','ClienteNatural14',487+648),
+('J34345678',34345678,'01-00000008','cliente15@gmail.com','José','Gonzalez','Lara','ClienteNatural15',487+648),
+('J35345678',35345678,'02-00000008','cliente16@gmail.com','José','Perez','Quevedo','ClienteNatural16',487+648),
+('J36345678',36345678,'01-00000009','cliente17@gmail.com','José','Gonzalez','Chavez','ClienteNatural17',487+648),
+('J37345678',37345678,'02-00000009','cliente18@gmail.com','José','Almeida','Padron','ClienteNatural18',487+648),
+('J38345678',38345678,'01-00000010','cliente19@gmail.com','José','Torrealba','Padron','ClienteNatural19',487+648),
+('J39345678',39345678,'02-00000010','cliente20@gmail.com','José','Rodriguez','Teran','ClienteNatural20',487+648),
+
+('J26345601',26345601,'01-00000011','cliente21@gmail.com','Isabella','Cardenas','Altuve','ClienteNatural21',487+644),
+('J26345602',26345602,'02-00000011','cliente22@gmail.com','Isabella','Cardenas','Almeida','ClienteNatural22',487+644),
+('J26345603',26345603,'01-00000012','cliente23@gmail.com','Isabella','Contreras','Lopez','ClienteNatural23',487+644),
+('J26345604',26345604,'02-00000012','cliente24@gmail.com','Isabella','Contreras','Altuve','ClienteNatural24',487+644),
+('J26345605',26345605,'01-00000013','cliente25@gmail.com','Isabella','Contreras','Altuve','ClienteNatural25',487+644),
+('J26345606',26345606,'02-00000013','cliente26@gmail.com','Isabella','Peralta','Altuve','ClienteNatural26',487+644),
+('J26345607',26345607,'01-00000014','cliente27@gmail.com','Isabella','Ortiz','Rodriguez','ClienteNatural27',487+644),
+('J26345608',26345608,'02-00000014','cliente28@gmail.com','Isabella','Ortiz','Gonzalez','ClienteNatural28',487+644),
+('J26345609',26345609,'01-00000015','cliente29@gmail.com','Isabella','Torrealba','Padron','ClienteNatural29',487+644),
+('J26345610',26345610,'02-00000015','cliente30@gmail.com','Isabella','Ortiz','Altuve','ClienteNatural30',487+644),
+
+('J26345611',26345611,'01-00000016','cliente31@gmail.com','Andrea','Perez','Rodriguez','ClienteNatural31',487+637),
+('J26345612',26345612,'02-00000016','cliente32@gmail.com','Andrea','Hernandez','Rodriguez','ClienteNatural32',487+637),
+('J26345613',26345613,'01-00000017','cliente33@gmail.com','Andrea','Lozano','Rodriguez','ClienteNatural33',487+637),
+('J26345614',26345614,'02-00000017','cliente34@gmail.com','Andrea','Hernandez','Pereira','ClienteNatural34',487+637),
+('J26345615',26345615,'01-00000018','cliente35@gmail.com','Andrea','Contreras','Gonzalez','ClienteNatural35',487+637),
+('J26345616',26345616,'02-00000018','cliente36@gmail.com','Andrea','Hernandez','Perez','ClienteNatural36',487+637),
+('J26345617',26345617,'01-00000019','cliente37@gmail.com','Andrea','Lopez','Gonzalez','ClienteNatural37',487+637),
+('J26345618',26345618,'02-00000019','cliente38@gmail.com','Andrea','Lopez','Almeida','ClienteNatural38',487+637),
+('J26345619',26345619,'01-00000020','cliente39@gmail.com','Andrea','Hernandez','Torrealba','ClienteNatural39',487+637),
+('J26345620',26345620,'02-00000020','cliente40@gmail.com','Andrea','Fernandez','Rodriguez','ClienteNatural40',487+637),
+
+('J26345621',26345621,'01-00000021','cliente41@gmail.com','Carlos','Rodriguez','Altuve','ClienteNatural41',487+807),
+('J26345622',26345622,'02-00000021','cliente42@gmail.com','Carlos','Rodriguez','Almeida','ClienteNatural42',487+807),
+('J26345623',26345623,'01-00000022','cliente43@gmail.com','Carlos','Rodriguez','Lopez','ClienteNatural43',487+807),
+('J26345624',26345624,'02-00000022','cliente44@gmail.com','Carlos','Pereira','Altuve','ClienteNatural44',487+807),
+('J26345625',26345625,'01-00000023','cliente45@gmail.com','Carlos','Gonzalez','Altuve','ClienteNatural45',487+807),
+('J26345626',26345626,'02-00000023','cliente46@gmail.com','Carlos','Perez','Altuve','ClienteNatural46',487+807),
+('J26345627',26345627,'01-00000024','cliente47@gmail.com','Carlos','Gonzalez','Rodriguez','ClienteNatural47',487+807),
+('J26345628',26345628,'02-00000024','cliente48@gmail.com','Carlos','Almeida','Gonzalez','ClienteNatural48',487+807),
+('J26345629',26345629,'01-00000025','cliente49@gmail.com','Carlos','Torrealba','Padron','ClienteNatural49',487+807),
+('J26345630',26345630,'02-00000025','cliente50@gmail.com','Carlos','Rodriguez','Altuve','ClienteNatural50',487+807),
+
+('J26345631',26345631,'01-00000026','cliente51@gmail.com','Luis','Rodriguez','Altuve','ClienteNatural51',487+829),
+('J26345632',26345632,'02-00000026','cliente52@gmail.com','Luis','Rodriguez','Almeida','ClienteNatural52',487+829),
+('J26345633',26345633,'01-00000027','cliente53@gmail.com','Luis','Rodriguez','Lopez','ClienteNatural53',487+829),
+('J26345634',26345634,'02-00000027','cliente54@gmail.com','Luis','Pereira','Altuve','ClienteNatural54',487+829),
+('J26345635',26345635,'01-00000028','cliente55@gmail.com','Luis','Gonzalez','Altuve','ClienteNatural55',487+829),
+('J26345636',26345636,'02-00000028','cliente56@gmail.com','Luis','Perez','Altuve','ClienteNatural56',487+829),
+('J26345637',26345637,'01-00000029','cliente57@gmail.com','Luis','Gonzalez','Rodriguez','ClienteNatural57',487+829),
+('J26345638',26345638,'02-00000029','cliente58@gmail.com','Luis','Almeida','Gonzalez','ClienteNatural58',487+829),
+('J26345639',26345639,'01-00000030','cliente59@gmail.com','Luis','Torrealba','Padron','ClienteNatural59',487+829),
+('J26345640',26345640,'02-00000030','cliente60@gmail.com','Luis','Rodriguez','Altuve','ClienteNatural60',487+829),
+
+('J26345641',26345641,'01-00000031','cliente61@gmail.com','Juan','Rodriguez','Altuve','ClienteNatural61',487+796),
+('J26345642',26345642,'02-00000031','cliente62@gmail.com','Juan','Rodriguez','Almeida','ClienteNatural62',487+796),
+('J26345643',26345643,'01-00000032','cliente63@gmail.com','Juan','Rodriguez','Lopez','ClienteNatural63',487+796),
+('J26345644',26345644,'02-00000032','cliente64@gmail.com','Juan','Pereira','Altuve','ClienteNatural64',487+796),
+('J26345645',26345645,'01-00000033','cliente65@gmail.com','Juan','Gonzalez','Altuve','ClienteNatural65',487+796),
+('J26345646',26345646,'02-00000033','cliente66@gmail.com','Juan','Perez','Altuve','ClienteNatural66',487+796),
+('J26345647',26345647,'01-00000034','cliente67@gmail.com','Juan','Gonzalez','Rodriguez','ClienteNatural67',487+796),
+('J26345648',26345648,'02-00000034','cliente68@gmail.com','Juan','Almeida','Gonzalez','ClienteNatural68',487+796),
+('J26345649',26345649,'01-00000035','cliente69@gmail.com','Juan','Torrealba','Padron','ClienteNatural69',487+796),
+('J26345650',26345650,'02-00000035','cliente70@gmail.com','Juan','Rodriguez','Altuve','ClienteNatural70',487+796),
+
+('J26345651',26345651,'01-00000036','cliente71@gmail.com','Valentina','Rodriguez','Altuve','ClienteNatural71',487+958),
+('J26345652',26345652,'02-00000036','cliente72@gmail.com','Valentina','Rodriguez','Almeida','ClienteNatural72',487+958),
+('J26345653',26345653,'01-00000037','cliente73@gmail.com','Valentina','Rodriguez','Lopez','ClienteNatural73',487+958),
+('J26345654',26345654,'02-00000037','cliente74@gmail.com','Valentina','Pereira','Altuve','ClienteNatural74',487+958),
+('J26345655',26345655,'01-00000038','cliente75@gmail.com','Valentina','Gonzalez','Altuve','ClienteNatural75',487+958),
+('J26345656',26345656,'02-00000038','cliente76@gmail.com','Valentina','Perez','Altuve','ClienteNatural76',487+958),
+('J26345657',26345657,'01-00000039','cliente77@gmail.com','Valentina','Gonzalez','Rodriguez','ClienteNatural77',487+958),
+('J26345658',26345658,'02-00000039','cliente78@gmail.com','Valentina','Almeida','Gonzalez','ClienteNatural78',487+958),
+('J26345659',26345659,'01-00000040','cliente79@gmail.com','Valentina','Torrealba','Padron','ClienteNatural79',487+958),
+('J26345660',26345660,'02-00000040','cliente80@gmail.com','Valentina','Rodriguez','Altuve','ClienteNatural80',487+958);
 
 
 
