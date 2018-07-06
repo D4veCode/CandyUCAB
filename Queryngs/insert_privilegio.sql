ca69b046bedf5389636bb58d1c89319da36a4d0d
Franco Marino <franco2662@gmail.com>
2018-07-06 01:30:06 -0400

    Inserts de cheque,rol,privilegio y priv-rol

---
+++ Queryngs/insert_privilegio.sql
@@ -0,0 +1,9 @@
+insert into privilegio (id, nombre) values (1,'Administrar datos de empleados');
+insert into privilegio (id, nombre) values (2, 'Administrar datos de clientes');
+insert into privilegio (id, nombre) values (3, 'Verificar asistencias ');
+insert into privilegio (id, nombre) values (4, 'Ingresar asistencia ');
+insert into privilegio (id, nombre) values (5, 'Asignar/Verificar vacaciones');
+insert into privilegio (id, nombre) values (6, 'Verificar inventario');
+insert into privilegio (id, nombre) values (7, 'Venta de productos');
+insert into privilegio (id, nombre) values (8, 'Ver historial de ventas');
+
